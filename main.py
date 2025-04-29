2d52e031575c72a1f5f975f359ad870e9adaddcd
tobiao <tobiasomdal@gmail.com>
2025-04-29 21:02:58 +0200

    Add: mouse events, battle zones and menus

--- main.py
+++ main.py
@@ -19,13 +19,15 @@ menu = [pygame.Rect(800, 500, menu_width, menu_height),
         pygame.Rect(800, 600, menu_width, menu_height), 
         pygame.Rect(1000, 600, menu_width, menu_height)] 
 
+battle_ground_player = pygame.Rect(200, 400, 400, 200)
+battle_ground_opponent = pygame.Rect(800, 200, 400, 160)
 
 def render():
     screen.fill("purple")
     pygame.draw.circle(screen, "white", player_pos, 20)
     
-    pygame.draw.circle(screen, "green", player_zone, 100)
-    pygame.draw.circle(screen, "green", opponent_zone, 100)
+    pygame.draw.ellipse(screen, "green", battle_ground_player, 0)
+    pygame.draw.ellipse(screen, "green", battle_ground_opponent, 0)
 
     pygame.draw.rect(screen, "grey", menu[0])
     pygame.draw.rect(screen, "grey", menu[1])
@@ -35,7 +37,7 @@ def render():
     font = pygame.font.Font('freesansbold.ttf', 32)
     menu1 = font.render("Fight", True, (255, 255, 255), 'grey')
     menu2 = font.render("Team", True, (255, 255, 255), 'grey')
-    menu3 = font.render("Center", True, (255, 255, 255), 'grey')
+    menu3 = font.render("Settings", True, (255, 255, 255), 'grey')
     menu4 = font.render("Flee", True, (255, 255, 255), 'grey')
 
     menu1_rect = menu1.get_rect()
@@ -61,20 +63,33 @@ def update():
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
-    elif keys[pygame.K_s]:
+    if keys[pygame.K_s]:
         player_pos.y += 300 * dt
-    elif keys[pygame.K_a]:
+    if keys[pygame.K_a]:
         player_pos.x -= 300 * dt
-    elif keys[pygame.K_d]:
+    if keys[pygame.K_d]:
         player_pos.x += 300 * dt
 
     dt = clock.tick(60) / 1000
+
+def attack():
+    print("ATTACK")
 
 def processEvents():
     global running
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                if x > 800 and x < 800 + menu_width and y > 500 and y < 500 + menu_height:
+                    attack()
+                elif x > 1000 and x < 1000 + menu_width and y > 500 and y < 500 + menu_height:
+                    print("Team")
+                elif x > 800 and x < 800 + menu_width and y > 600 and y < 600 + menu_height:
+                    print("Settings")
+                elif x > 1000 and x < 1000 + menu_width and y > 600 and y < 600 + menu_height:
+                    print("Flee")
 
 def main_loop():
     while running:
